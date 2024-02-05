57b65d8a8359d6cf800ad7be13cd80a3bab0f495
Montes15 <samuelmontes1105@gmail.com>
2024-02-05 17:35:17 -0500

    feat: home slider created

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -20,6 +20,16 @@ export default function Home() {
           <Button content="🌙" className="dark-button"/>
         </section>
       </header>
+      <main className="main-home">
+          <section className="slider">
+              <img className="slider-image" src="/Images/bus1.jpeg"/>
+              <img className="slider-image" src="/Images/bus2.jpeg"/>
+              <img className="slider-image" src="/Images/bus3.jpeg"/>
+              <img className="slider-image" src="/Images/bus4.jpeg"/>
+              <img className="slider-image" src="/Images/bus5.jpeg"/>
+              <img className="slider-image" src="/Images/bus6.jpeg"/>
+          </section>
+      </main>
     </>
   )
 }
