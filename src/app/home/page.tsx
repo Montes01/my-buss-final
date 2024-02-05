f24b84b7a3d1826bc619632d1cbc964a31d741e1
Montes <samuelmontes1105@gmail.com>
2024-02-05 10:53:12 -0500

    feat: started header in home page

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,10 +1,16 @@
 import Button from "@/system-design/atoms/Button";
+import "./_home.scss";
 export default function Home() {
 
 
   return (
     <>
-      <Button content="this is a button"/>
-    </>    
+      <header className="home-header">
+        <Button content="Inicio" />
+        <Button content="Ingresa" />
+        <Button content="Registro" />
+        <Button content="Nosotros" />
+      </header>
+    </>
   )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,13 @@
-import Button from "@/system-design/atoms/Button";
+
 import Input from "@/system-design/atoms/Input";
 
 export default function Home() {
   return (
-    <main className="">
-        <Button content="diavlo" />
-        <Input label="ejemplo" />
-        <Input type="radio" label="hola"/>
-    </main>
+    <>
+      
+    
+    
+    
+    </>
   );
 }
