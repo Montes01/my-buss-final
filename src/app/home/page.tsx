2d13eb73defefdedcb4149d26ac6272ffa8fa4d9
Montes15 <samuelmontes1105@gmail.com>
2024-02-05 21:17:59 -0500

    feat: add  home main buttons feat: added secondary color

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,13 +1,23 @@
-
 import { PRINCIPAL_MESSAGE } from "@/lib/constants/constants";
+import { IconBrandAndroid, IconMap } from "@tabler/icons-react"
 import "./_home.scss";
+import NavButton from "@/system-design/atoms/NavButton";
 export default function Home() {
 
 
   return (
     <section className="home-content">
       <p className="principal-message">{PRINCIPAL_MESSAGE}</p>
-
+      <section className="main-buttons">
+        <NavButton to="/App" className="button main-button">
+          <IconBrandAndroid color="white" />
+          <strong>Descarga nuestra aplicación</strong>
+        </NavButton>
+        <NavButton to="/home" className="button main-button">
+          <IconMap color="white" />
+          <strong>conoce nuestras rutas</strong>
+        </NavButton>
+      </section>
     </section>
   )
 }
