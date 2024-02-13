da162b35e6e018232c47c85adc1988dc73a608cd
Montes15 <samuelmontes1105@gmail.com>
2024-02-13 18:45:14 -0500

    feat: Update header buttons in dashboard.scss and Header.tsx

--- src/app/dashboard/shared/Header.tsx
+++ src/app/dashboard/shared/Header.tsx
@@ -1,7 +1,7 @@
 import Logo from "@/system-design/molecules/Logo";
 import NavButton from "@/system-design/atoms/NavButton";
 import Button from "@/system-design/atoms/Button";
-import {  IconLogout } from "@tabler/icons-react";
+import {  IconLogout, IconUser } from "@tabler/icons-react";
 export default function Header() {
 
     return (
@@ -9,9 +9,10 @@ export default function Header() {
             <Logo to="/dashboard" className="dashboard-logo"/>
             <section className="header-buttons">
                 <NavButton children="Inicio" className="header-button" to="/dashboard" />
-                <NavButton children="Perfil" className="header-button" to="/dashboard/profile" />
                 <NavButton children="Configuración" className="header-button" to="/dashboard/settings" />
-                <Button content={IconLogout} className="logout-button" />
+                {/* if image fails put another src */}
+                <Button content={IconUser} className="header-button" />
+                <Button content={IconLogout} className="header-button" />
             </section>
         </header>
     )
