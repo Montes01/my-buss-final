6b812fcf087645700c7561a17268b389a5f8e860
Montes15 <samuelmontes1105@gmail.com>
2024-02-28 23:38:49 -0500

    feat: Remove unused imports and state in join layout component

--- src/app/home/join/layout.tsx
+++ src/app/home/join/layout.tsx
@@ -1,17 +1,11 @@
 'use client'
 import "./_join.scss"
-import NavButton from "@/system-design/atoms/NavButton"
-import { useEffect, useState } from "react"
 
 interface Props {
     children: React.ReactNode
 }
 export default function Layout({ children }: Props) {
 
-    const [isLogin, setIsLogin] = useState(false)
-    useEffect(() => {
-        setIsLogin(location.pathname.includes(`/join/login`))
-    }, [location.pathname])
 
     return (
         <main className="join-home">
