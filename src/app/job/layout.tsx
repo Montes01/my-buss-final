e2bc3bc78f433e9eab687064ad79d901b23940dc
Montes15 <samuelmontes1105@gmail.com>
2024-03-10 09:01:51 -0500

    feat: added provider to job page

--- src/app/job/layout.tsx
+++ src/app/job/layout.tsx
@@ -5,13 +5,15 @@ interface Props {
 
     children: React.ReactNode
 }
+import { Provider } from "react-redux";
+import { store } from "../../lib/context/store";
 
 export default function Layout({ children }: Props) {
     return (
-        <>
+        <Provider store={store}>
             <Header />
             {children}
             <Footer />
-        </>
-    )
+        </Provider>
+    );
 }
