8d5ad368c44e2208c2975c15bb9a460373850733
Montes15 <samuelmontes1105@gmail.com>
2024-03-13 02:13:43 -0500

    feat: Update RouteItem component to display route name

--- src/system-design/molecules/RouteItem.tsx
+++ src/system-design/molecules/RouteItem.tsx
@@ -1,13 +1,13 @@
 import { Ruta } from "@/lib/constants/declarations";
 import Button from "../atoms/Button";
 import { IconEdit, IconTrash } from "@tabler/icons-react"
-export default function RouteItem({ ID_Ruta }: Ruta) {
+export default function RouteItem({ ID_Ruta, Nombre }: Ruta) {
 
 
     return (
         <article className="route-item">
             <section className="route-state">
-                <strong className="route-number">Ruta {ID_Ruta}</strong>
+                <strong className="route-number">{Nombre}</strong>
             </section>
             <section className="controls">
                 <Button content={IconEdit} className="edit-button" />
