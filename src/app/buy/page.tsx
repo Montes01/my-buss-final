10de664d4ed70c304348c715bb9358172ca578ef
Montes15 <samuelmontes1105@gmail.com>
2024-03-17 11:48:07 -0500

    feat: Add client-side code to page.tsx

--- src/app/buy/page.tsx
+++ src/app/buy/page.tsx
@@ -1,3 +1,4 @@
+"use client"
 import { useState, useEffect } from "react";
 import { ENDPOINTS, PROJECT_NAME, SERVER_URL } from "@/lib/constants/constants";
 import { Empresa } from "@/lib/constants/declarations";
