0d604c71b0d3646adff26fea8854cc604fbd7db8
MaicolGARCIA1 <115556588+MaicolGARCIA1@users.noreply.github.com>
2024-03-18 22:44:38 -0500

    Manejo de apartado de paraderos

--- src/app/home/stops/page.tsx
+++ src/app/home/stops/page.tsx
@@ -20,19 +20,23 @@ export default async function Stops() {
 
     return (
         <main className="stops-page">
-            <h2 className="title">Paraderos de la ciudad</h2>
-            <section className="stop-list">
-                {stops.map(({ ID_Paradero, Nombre, Foto }) => (
-                    <section key={ID_Paradero} className="stop">
-                        <img src="https://media.wired.com/photos/59269cd37034dc5f91bec0f1/master/pass/GoogleMapTA.jpg" alt="" className="stop-location" />
-                        <section className="content">
-                            <h3>{Nombre}</h3>
-                            <img src={Foto} alt="" className="stop-image" />
-                            <NavButton to={`/home/stops/${ID_Paradero}`} content="Ver paradero" className="button" />
-                        </section>
-                    </section>
-                ))}
-            </section>
-        </main>
+  <div className="section-header">
+    <h2 className="title">Paraderos de la ciudad</h2>
+    <p className="subtitle">Explora los paraderos disponibles en nuestra ciudad</p>
+  </div>
+  <section className="stop-list">
+    {stops.map(({ ID_Paradero, Nombre, Foto }) => (
+      <section key={ID_Paradero} className="stop">
+        <img src="https://media.wired.com/photos/59269cd37034dc5f91bec0f1/master/pass/GoogleMapTA.jpg" alt="" className="stop-location" />
+        <div className="content">
+          <h3>{Nombre}</h3>
+          <img src={Foto} alt="" className="stop-image" />
+          <NavButton to={`/home/stops/${ID_Paradero}`} content="Ver paradero" className="button" />
+        </div>
+      </section>
+    ))}
+  </section>
+</main>
+
     )
 }   
