cd5ad9d50043bfab009c9db2ef81866a7b7bd267
Montes15 <samuelmontes1105@gmail.com>
2024-03-21 13:21:16 -0500

    Update variable names in CompanyCard component

--- src/system-design/molecules/CompanyCard.tsx
+++ src/system-design/molecules/CompanyCard.tsx
@@ -2,18 +2,18 @@ import { useRef } from "react";
 import { Empresa } from "@/lib/constants/declarations";
 import Button from "../atoms/Button";
 
-export default function CompanyCard({ nombre, logo }: Empresa) {
+export default function CompanyCard({ Nombre, Logo }: Empresa) {
     const imageRef = useRef<HTMLImageElement>(null)
     return (
         <article className="company-card">
-            <img ref={imageRef} className="company-image" width={250} height={250} src={logo} onError={() => {
+            <img ref={imageRef} className="company-image" width={250} height={250} src={Logo} onError={() => {
                 if (imageRef.current) {
                     imageRef.current.src = "/Images/user.png"
                 }
             }} alt="" />
 
             <section className="company-content">
-                <h3 className="company-name">{nombre}</h3>
+                <h3 className="company-name">{Nombre}</h3>
                 <Button content="ver rutas" className="ver-rutas-button" />
             </section>
 
