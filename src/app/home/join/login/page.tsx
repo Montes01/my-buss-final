e73c025ea13ab73d0ec56f147b0d23accc7dc485
Montes15 <samuelmontes1105@gmail.com>
2024-03-22 03:38:08 -0500

    Refactor login page and parse company list

--- src/app/home/join/login/page.tsx
+++ src/app/home/join/login/page.tsx
@@ -8,12 +8,8 @@ import { UsePost } from "@/lib/hooks/fetchHook"
 import Spinner from "@/system-design/atoms/Spinner";
 import { useState } from "react";
 import ChangeJoin from "../shared/ChanngeJoin";
-import { useRouter } from "next/navigation";
-import { loginMock } from "@/lib/constants/mocks";
-import { Response } from "@/lib/constants/declarations";
 import Link from "next/link";
 export default function Login() {
-    const router = useRouter()
     const [error, setError] = useState("")
     const [isLoading, setIsLoading] = useState(false)
     const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
@@ -27,7 +23,9 @@ export default function Login() {
         try {
             const res = await UsePost(SERVER_URL + ENDPOINTS.USER.LOGIN, body);
             localStorage.setItem('user-token', res?.Data);
-            router.push('/home');
+            if (location) {
+                location.href = "/home"
+            }
         } catch (err: unknown) {
             setError("Correo o contraseña incorrectos")
         }
@@ -37,25 +35,25 @@ export default function Login() {
         <>
             <ChangeJoin isLogin />
             <form onSubmit={handleSubmit} action="" className="login-form">
-    <h3 className="login-form_title">Inicia Sesión</h3>
-    <section className="login-ways">
-        {/* Aquí podrías agregar botones para iniciar sesión con Google, Facebook, etc. */}
-    </section>
-    <section className="camps">
-        <Input required name="email" className="form-input" label="Correo" type="email" />
-        <Input required name="password" className="form-input" label="Contraseña" type="password" />
-        <section className="final-step">
-            <Button submit content="Ingresar" className="login-button" />
-            {isLoading && <Spinner />}
-        </section>
-        <section className="join-as-company">
-            <p>¿Eres una empresa? <Link className="company-anchor" href="/home/join/company">Ingresa a tu cuenta</Link> </p>
-        </section>
-        <section className="login-form_error">
-            {error}
-        </section>
-    </section>
-</form>
+                <h3 className="login-form_title">Inicia Sesión</h3>
+                <section className="login-ways">
+                    {/* Aquí podrías agregar botones para iniciar sesión con Google, Facebook, etc. */}
+                </section>
+                <section className="camps">
+                    <Input required name="email" className="form-input" label="Correo" type="email" />
+                    <Input required name="password" className="form-input" label="Contraseña" type="password" />
+                    <section className="final-step">
+                        <Button submit content="Ingresar" className="login-button" />
+                        {isLoading && <Spinner />}
+                    </section>
+                    <section className="join-as-company">
+                        <p>¿Eres una empresa? <Link className="company-anchor" href="/home/join/company">Ingresa a tu cuenta</Link> </p>
+                    </section>
+                    <section className="login-form_error">
+                        {error}
+                    </section>
+                </section>
+            </form>
 
         </>
     )
