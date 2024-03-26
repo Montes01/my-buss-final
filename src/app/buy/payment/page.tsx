da98e738e3b7f61b67d0cafb2f18d7932074cbb9
Montes15 <samuelmontes1105@gmail.com>
2024-03-26 13:09:04 -0500

    payment method working, admin layout just created

--- src/app/buy/payment/page.tsx
+++ src/app/buy/payment/page.tsx
@@ -1,22 +1,24 @@
 "use client"
-import { ENDPOINTS, SERVER_URL } from '@/lib/constants/constants';
-import { Empresa, Ticket } from '@/lib/constants/declarations';
+import { ENDPOINTS, SERVER_URL, STRIPE_API_KEY } from '@/lib/constants/constants';
+import { CardElement, Elements, useElements, useStripe } from '@stripe/react-stripe-js';
 import { parseCompany, parseSingleTicket } from '@/lib/constants/utils';
-import { UseGet, UsePost, UsePut } from '@/lib/hooks/fetchHook';
 import { useEffect, useRef, useState } from 'react';
+import { Empresa, Ticket } from '@/lib/constants/declarations';
+import { UseAppSelector } from '@/lib/context/hooks';
+import { UseGet, UsePut } from '@/lib/hooks/fetchHook';
+import { loadStripe } from '@stripe/stripe-js';
 import { useRouter } from 'next/navigation';
 import swal from 'sweetalert';
-import { UseAppSelector } from '@/lib/context/hooks';
-import Input from '@/system-design/atoms/Input';
 import Button from '@/system-design/atoms/Button';
-import "./payment.scss"
-const Page = () => {
+
+export default function PaymentPage() {
+  const elements = useElements();
+  const stripe = useStripe()
   const [IdTicket, setIdTicket] = useState(null as number | null);
   const [Ticket, setTicket] = useState({} as Ticket);
   const [company, setCompany] = useState({} as Empresa)
   const user = UseAppSelector(state => state.user);
   const router = useRouter();
-  const dialogRef = useRef<HTMLDialogElement>(null);
   useEffect(function () {
     if (location) {
       var searchParams = new URLSearchParams(location.search);
@@ -65,65 +67,52 @@ const Page = () => {
 
   }, [Ticket])
 
-  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault()
-    dialogRef.current?.showModal()
-  }
-  const handleConfirm = async () => {
-    const token = localStorage.getItem("user-token");
-    const data = Ticket
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
     try {
-      await UsePut(SERVER_URL + ENDPOINTS.TICKET.PAY, data, { Authorization: `Bearer ${token}` })
-      swal("Pago exitoso", "Tu ticket ha sido pagado con éxito", "success")
-      router.push("/home/profile")
+      const paymentMethodResult = await stripe?.createPaymentMethod({
+        type: 'card',
+        card: elements?.getElement(CardElement)!,
+
+      });
+      if (paymentMethodResult?.error) {
+        swal("Error", paymentMethodResult.error.message!, "error");
+        return;
+      }
+
+      const token = localStorage.getItem("user-token");
+      const data = {
+        ID_Ticket: Ticket.ID_Ticket,
+        Precio: Ticket.Precio,
+        Estado: "Pagado",
+        TipoPago: "Tarjeta",
+        ID_Pago: paymentMethodResult?.paymentMethod?.id,
+        ID_Empresa: Ticket.ID_Empresa
+      };
+      await UsePut(SERVER_URL + ENDPOINTS.TICKET.PAY, data, {
+        Authorization: `Bearer ${token}`
+      });
+      swal("Pago exitoso", "Su ticket ha sido pagado exitosamente", "success");
+      router.push("/home/profile");
     } catch (error) {
       console.error(await error);
-      swal("Error", "No se ha podido realizar el pago", "error")
+      swal("Error", "No se ha podido realizar el pago", "error");
     }
   }
 
   return (<>
-    <main className='ticket-payment-main'>
-      <form onSubmit={handleSubmit} className="ticket-pay-form">
-        <fieldset>
-          <legend>Paga tu ticket aqui</legend>
-          <Input
-            pattern='^3[47][0-9]{13}$'
-            required
-            label='Tarjeta de debito/crédito'
-            placeholder='0000-0000-0000-0000'
-            name='Tarjeta de credito'
-            type='number' />
-          <Input
-            required
-            label='Fecha de expiración'
-            placeholder='MM/YY'
-            name='Fecha de expiración'
-            type='number' />
-          <Input
-            required
-            label='CVV'
-            placeholder='000'
-            name='CVV'
-            type='number' />
-        </fieldset>
-        <Button submit content={Ticket.Precio + "$"} />
-      </form>
-      <section className="pay-ticket-image">
-        <img src={company?.Logo} alt="" />
-      </section>
-    </main>
-    <dialog ref={dialogRef} className='confirm-buy-dialog'>
-      <h1>Confirma tu pago</h1>
-      <p>¿Estas seguro de pagar este ticket?</p>
-      <div className="dialog-pay-buttons">
-
-        <Button content='Pagar' action={handleConfirm} />
-        <Button content='Cancelar' submit />
-      </div>
-    </dialog>
+    <form onSubmit={handleSubmit} className="ticket-pay-form">
+      <fieldset>
+        <legend>Paga tu ticket aqui</legend>
+        <h2 className='company-name'>{company.Nombre}</h2>
+        <section className="pay-ticket-image">
+          <img src={company?.Logo} alt="" />
+        </section>
+        <CardElement className='credit-card' />
+      </fieldset>
+      <Button submit content={Ticket.Precio + "$"} />
+    </form>
   </>
   );
 }
 
-export default Page;
